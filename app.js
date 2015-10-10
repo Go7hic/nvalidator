ffe24647822cd29a6449800fd6bd6df46fe91e27
Go7hic <gtfx0209@qq.com>
2015-10-10 16:55:39 +0800

    添加 app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+/**
+ * node validator example
+ * @type {[type]}
+ */
 var validator = require('./lib');
-
 // isEmail
 console.log(validator.isEmail('test@gmail.com'));
+// is mobile
+console.log(validator.isMobile(15867152502));
+// is all english
+console.log(validator.isAllEnglish('hello'));
+
+// is all chinese
+console.log(validator.isAllChinese('你们好'));
